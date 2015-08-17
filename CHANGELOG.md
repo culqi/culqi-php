2b0a20a90924b575aaebce7f029d71cdf407875e
William Muro <william.muro@culqi.com>
2015-08-17 10:38:14 -0500

    Nuevos archivos.

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+
+### 1.0.0 17-08-2015
+
+* Primera versión de la librería, soporta las operaciones de Anulación, Consulta, Autorización.
---
+++ culqi_test.php
@@ -0,0 +1,29 @@
+<?php
+
+require 'culqi_src.php';
+
+Culqi::$llaveSecreta = "zzmxZlgIJtKKy0F71DMsZPWnPVzow4S90abBScLDIrk=";
+Culqi::$codigoComercio = "testc101";
+Culqi::$servidorBase = 'https://devpago.culqi.com';
+try {
+//    $data = Pago::crearDatospago(array(
+//        Pago::PARAM_NUM_PEDIDO => "tlvh20150727-1",
+//        Pago::PARAM_MONEDA => "PEN",
+//        Pago::PARAM_MONTO => "123",
+//    ));
+//    $informacionVenta = $data[Pago::PARAM_INFO_VENTA];
+//    $dataDecifrada = Culqi::decifrar($informacionVenta);
+//
+//    echo "Información de la venta: $informacionVenta\n";
+//    var_dump($dataDecifrada);
+//    $data = Pago::consultar("0MXpbwlGjRU9Sr0IwIOqHh1aVJICjGh9KIq");
+    $data = Pago::anular("0MXpbwlGjRU9Sr0IwIOqHh1aVJICjGh9KIq");
+    $data = Pago::consultar("0MXpbwlGjRU9Sr0IwIOqHh1aVJICjGh9KIq");
+    $codigoRespuesta = $data['codigo_respuesta'];
+    echo $codigoRespuesta;
+    var_dump($data);
+
+
+} catch (InvalidParamsException $e) {
+    echo $e->getMessage()."\n";
+}
