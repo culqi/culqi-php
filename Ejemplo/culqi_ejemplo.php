c22acf711faa282053819c38a817672926644d56
William Muro <william.muro@culqi.com>
2015-08-17 13:53:53 -0500

    Nuevo ejemplo.

--- Ejemplo/culqi_ejemplo.php
+++ Ejemplo/culqi_ejemplo.php
@@ -1,37 +1,49 @@
 <?php
+    require 'culqi.php';
+    
+    Culqi::$llaveSecreta = "zzmxZlgIJtKKy0F71DMsZPWnPVzow4S90abBScLDIrk";
+    Culqi::$codigoComercio = "testc101";
+    Culqi::$servidorBase = 'https://devpago.culqi.com';
+    
+    try {
+        
+        $data = Pago::crearDatospago(array(
+                                           
+            //Numero de pedido de la venta
+            Pago::PARAM_NUM_PEDIDO => "0001",
+                                           
+            //Moneda de la venta ("PEN" O "USD")
+            Pago::PARAM_MONEDA => "PEN",
+                                           
+            //Monto de la venta (ejem: 10.25, va sin el punto decimal)
+            Pago::PARAM_MONTO => "1025",
+                                           
+            //Descripción de la venta
+            Pago::PARAM_DESCRIPCION => "123",
+                                           
+            //Código del país del cliente Ej. PE, US
+            Pago::PARAM_COD_PAIS => "PE",
+                                           
+            //Ciudad del cliente
+            Pago::PARAM_CIUDAD => "Lima",
+                                           
+            //Dirección del cliente
+            Pago::PARAM_DIRECCION => "Avenida Lima 2132, Miradores",
+                                           
+            //Número de teléfono del cliente
+            Pago::PARAM_NUM_TEL => "992765900",
+                                           
+        ));
+        
+        //Respuesta de la creación de la venta. Cadena cifrada.
+        $informacionVenta = $data[Pago::PARAM_INFO_VENTA];
+        
+        echo "<script src=\"https://devpago.culqi.com/culqi.js\"></script><script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js\"></script><button id=\"btn_pago\">Pagar</button><script>checkout.codigo_comercio = \"testc101\";checkout.informacion_venta = \"$informacionVenta\";$('#btn_pago').on('click', function(e) {checkout.abrir();e.preventDefault();});function culqi (checkout) {console.log(checkout.respuesta);checkout.cerrar();};</script>";
 
-require 'culqi_src.php';
 
-Culqi::$llaveSecreta = "zzmxZlgIJtKKy0F71DMsZPWnPVzow4S90abBScLDIrk=";
-Culqi::$codigoComercio = "testc101";
-Culqi::$servidorBase = 'https://integ-pago.culqi.com';
-    
-try {
-    
-//    $data = Pago::crearDatospago(array(
-//        Pago::PARAM_NUM_PEDIDO => "tlvh20150727-1",
-//        Pago::PARAM_MONEDA => "PEN",
-//        Pago::PARAM_MONTO => "123",
-//    ));
-    
-//    $informacionVenta = $data[Pago::PARAM_INFO_VENTA];
-    
-//    $dataDecifrada = Culqi::decifrar($informacionVenta);
-//
-//    echo "Información de la venta: $informacionVenta\n";
-//    var_dump($dataDecifrada);
-//    $data = Pago::consultar("0MXpbwlGjRU9Sr0IwIOqHh1aVJICjGh9KIq");
-    
-    $data = Pago::anular("0MXpbwlGjRU9Sr0IwIOqHh1aVJICjGh9KIq");
-    
-    $data = Pago::consultar("0MXpbwlGjRU9Sr0IwIOqHh1aVJICjGh9KIq");
-    
-    $codigoRespuesta = $data['codigo_respuesta'];
-    
-    echo $codigoRespuesta;
-    var_dump($data);
-
-
-} catch (InvalidParamsException $e) {
-    echo $e->getMessage()."\n";
-}
+    } catch (InvalidParamsException $e) {
+        
+        echo $e->getMessage()."\n";
+        
+    }
+?>
