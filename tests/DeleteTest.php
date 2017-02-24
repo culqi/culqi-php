b6d0eaa8ad4b0e71772f3a77e11259299b60b933
Willy Aguirre <marti1125@gmail.com>
2017-02-24 12:39:24 -0500

    add DeleteTest.php

--- tests/DeleteTest.php
+++ tests/DeleteTest.php
@@ -8,10 +8,53 @@ class DeleteTest extends PHPUnit_Framework_TestCase
 {
 
     protected $API_KEY;
+    protected $PUBLIC_API_KEY;
 
     protected function setUp() {
-        $this->API_KEY = getenv("API_KEY");
+        $this->PUBLIC_API_KEY = getenv("PUBLIC_API_KEY");
+        $this->API_KEY =  getenv("API_KEY");
+        $this->culqi_token = new Culqi(array("api_key" => $this->PUBLIC_API_KEY ));
         $this->culqi = new Culqi(array("api_key" => $this->API_KEY ));
+    }
+
+    protected function createToken() {
+        $token = $this->culqi_token->Tokens->create(
+            array(
+                "card_number" => "4111111111111111",
+                "cvv" => "123",
+                "email" => "wmuro".uniqid()."@me.com",
+                "expiration_month" => 9,
+                "expiration_year" => 2020,
+                "fingerprint" => "q352454534"
+            )
+        );
+        return $token;
+    }
+
+    public function createCustomer() {
+        $customer = $this->culqi->Customers->create(
+            array(
+                "address" => "av lima 123",
+                "address_city" => "lima",
+                "country_code" => "PE",
+                "email" => "www@".uniqid()."me.com",
+                "first_name" => "Will",
+                "last_name" => "Muro",
+                "metadata" => array("test"=>"test"),
+                "phone_number" => 899898999
+            )
+        );
+        return $customer;
+    }
+
+    public function createCard() {
+        $card = $this->culqi->Cards->create(
+            array(
+                "customer_id" => $this->createCustomer()->id,
+                "token_id" => $this->createToken()->id
+            )
+        );
+        return $card;
     }
 
     public function createPlan() {
@@ -29,13 +72,34 @@ class DeleteTest extends PHPUnit_Framework_TestCase
         return $plan;
     }
 
-    public function testCreatePlan() {
-        $this->assertEquals('plan', $this->createPlan()->object);
+    public function createSubscription() {
+        $subscription = $this->culqi->Subscriptions->create(
+            array(
+                "card_id" => $this->createCard()->id,
+                "plan_id" => $this->createPlan()->id
+            )
+        );
+        return $subscription;
+    }
+
+    public function testDeleteSubscription() {
+        $subscriptionDeleted = $this->culqi->Subscriptions->delete($this->createSubscription()->id);
+        $this->assertTrue($subscriptionDeleted->deleted);
     }
 
     public function testDeletePlan() {
         $planDeleted = $this->culqi->Plans->delete($this->createPlan()->id);
         $this->assertTrue($planDeleted->deleted);
+    }
+
+    public function testDeleteCard() {
+        $cardDeleted = $this->culqi->Cards->delete($this->createCard()->id);
+        $this->assertTrue($cardDeleted->deleted);
+    }
+
+    public function testDeleteCustomer() {
+        $customerDeleted = $this->culqi->Customers->delete($this->createCustomer()->id);
+        $this->assertTrue($customerDeleted->deleted);
     }
 
 }
