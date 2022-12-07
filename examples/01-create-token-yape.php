38ca72df7f4af6cd8224fd366c94575bf8142a92
JoseHCalderon <105760917+JoseHCalderon@users.noreply.github.com>
2022-12-06 22:08:10 -0500

    tkn-yape

---
+++ examples/01-create-token-yape.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Ejemplo 1
+ * Como crear un token Yape con Culqi PHP.
+ */
+
+try {
+  // Usando Composer (o puedes incluir las dependencias manualmente)
+  require '../vendor/autoload.php';
+
+  // Codigo de Comercio
+  $PUBLIC_KEY = "{PUBLIC KEY}";
+  $culqi = new Culqi\Culqi(array('api_key' => $PUBLIC_KEY));
+  
+  // Creando Cargo a una tarjeta
+  $token = $culqi->Tokens->createYape(
+      array(
+        "number_phone" => "900000001",
+        "otp" => "425251",
+        "amount" => 700,
+        "metadata" => array("dni" => "71702935")
+      )
+  );
+  // Respuesta
+  echo json_encode("Token: ".$token->id);
+
+} catch (Exception $e) {
+  echo json_encode($e->getMessage());
+}
